cea1a6032c72b66f404c8bad2ca6582d4beefa05
MIBCS <aryanjadhavart2005@gmail.com>
2026-05-04 12:43:35 +0530

    Doing some admin cleanup

--- bakery_platform/users/admin.py
+++ bakery_platform/users/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
-from .models import UserProfile
+from django.contrib.auth.models import User
+from .models import *
 
+# ✅ Register default Django User safely
+admin.site.register(User)
+
+# ✅ Register all your models
 admin.site.register(UserProfile)
+admin.site.register(Order)
+admin.site.register(Course)
+admin.site.register(Product)
+admin.site.register(Enrollment)
+admin.site.register(ChatMessage)
+admin.site.register(ClassSchedule)
