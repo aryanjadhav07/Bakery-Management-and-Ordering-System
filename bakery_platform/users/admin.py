b5787d0b45ef294323c1c50482b8268cd9557f8f
MIBCS <aryanjadhavart2005@gmail.com>
2026-05-04 12:56:22 +0530

    Testing

--- bakery_platform/users/admin.py
+++ bakery_platform/users/admin.py
@@ -1,15 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth.models import User
-from .models import *
+from .models import UserProfile
 
-# ✅ Register default Django User safely
-
-
-# ✅ Register all your models
 admin.site.register(UserProfile)
-admin.site.register(Order)
-admin.site.register(Course)
-admin.site.register(Product)
-admin.site.register(Enrollment)
-admin.site.register(ChatMessage)
-admin.site.register(ClassSchedule)
